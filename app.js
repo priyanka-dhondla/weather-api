87693d4aa71ff062c04f5f346fbf373a65e0d101
priyanka-dhondla <164379280+priyanka-dhondla@users.noreply.github.com>
2024-08-30 12:18:46 +0530

    changes done

--- app.js
+++ app.js
@@ -5,31 +5,32 @@ const port = 3000;
 
 const weatherstackApiKey = 'd08b0d08e9816b3ec22eaec954856195'; 
 
-
 app.get('/weather', async (req, res) => {
-    const location = req.query.location;
-  
-    if (!location) {
-      return res.status(400).json({ error: 'Please enter a valid location' });
-    }
-  
-    try {
-      const response = await axios.get(`http://api.weatherstack.com/current`, {
-        params: {
-          access_key: weatherstackApiKey,
-          query: location
-        }
+  const location = req.query.location;
+
+  if (!location) {
+      return res.status(400).json({ error: 'Location name is required in query parameter' });
+  }
+
+  try {
+      const response = await axios.get('http://api.weatherstack.com/current', {
+          params: {
+              access_key: weatherstackApiKey,
+              query: location
+          }
       });
-  
+
       
+      if (response.data.error) {
+          return res.status(400).json({ error: 'Please enter a valid location' });
+      }
+
       res.status(200).json(response.data);
-    } catch (error) {
+  } catch (error) {
       console.error('Error fetching weather data:', error.message);
       res.status(500).json({ error: 'Failed to fetch weather data' });
-    }
-  });
-  
-
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
